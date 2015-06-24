aa37a81c9bf97f74100bb78fa20b5fc65350b034
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2015-06-24 08:39:08 -0300

    add comments

--- app/src/main/java/com/github/douglasjunior/sampletracker/Terrain.java
+++ app/src/main/java/com/github/douglasjunior/sampletracker/Terrain.java
@@ -59,6 +59,7 @@ public class Terrain extends View {
         super.onDraw(canvas);
         Log.d(getClass().getSimpleName(), "onDraw");
 
+        // toma o centro do trator como sendo o "marco zero" do canvas.
         canvas.translate(center.x, center.y);
 
         if (trackerHistory.size() > 1) {
@@ -77,8 +78,6 @@ public class Terrain extends View {
                 path.lineTo((float) posTratorAtual[0], (float) posTratorAtual[1]);
             }
 
-            Vector dirFixed = new Vector(new double[]{0d, 1d});
-
             /*
             Após desenhar o caminho, pega somente o vetor da última posição e tenta encontrar o ângulo de rotação.
              */
@@ -96,36 +95,45 @@ public class Terrain extends View {
             dirTrator.normalize(deslocamento);
             Log.d(getClass().getSimpleName(), "dirTrator normalizado: " + dirTrator);
 
-            double senAngulo = posTratorPrev.cartesian(0) / deslocamento;
-            Log.d(getClass().getSimpleName(), "senAngulo: " + senAngulo);
+            /*
+            Tendo a última coordenada como um triângulo, 'posTratorPrev' guarda os valores dos catetos, enquanto 'deslocamento' é a hipotenusa
+             */
+            double degrees;
 
-            //double angulo = radToDeg(dirTrator.dot(dirFixed));
-            double angulo = Math.toDegrees(Math.asin(senAngulo));
-            Log.d(getClass().getSimpleName(), "angulo: " + angulo);
-
+            // se o deslocamento de Y for negativo, então o cateto oposto é o deslocamento de Y
             if (posTratorPrev.cartesian(1) < 0) {
+                // dividindo o cateto oposto pela hipetenusa temos o Seno do Angulo que deve ser rotacionado.
+                double sin = posTratorPrev.cartesian(1) / deslocamento;
+                Log.d(getClass().getSimpleName(), "senAngulo: " + sin);
+
+                // quando Y é negativo, é preciso somar ou subitrair 90 graus, dependendo do valor de X
                 if (posTratorPrev.cartesian(0) < 0) {
-                    senAngulo = posTratorPrev.cartesian(1) / deslocamento;
-                    Log.d(getClass().getSimpleName(), "senAngulo: " + senAngulo);
-
-                    angulo = Math.toDegrees(Math.asin(senAngulo)) - 90d;
-                    Log.d(getClass().getSimpleName(), "angulo: " + angulo);
+                    // a função Math.asin() calcula o arco radiano para o Seno calculado anteriormente.
+                    // e a função Math.toDegress() converte o radiano para graus de 0 a 360.
+                    degrees = Math.toDegrees(Math.asin(sin)) - 90d;
                 } else {
-                    senAngulo = posTratorPrev.cartesian(1) / deslocamento;
-                    Log.d(getClass().getSimpleName(), "senAngulo: " + senAngulo);
-
-                    angulo = Math.abs(Math.toDegrees(Math.asin(senAngulo))) + 90d;
-                    Log.d(getClass().getSimpleName(), "angulo: " + angulo);
+                    // a função Math.asin() calcula o arco radiano para o Seno calculado anteriormente.
+                    // a função Math.toDegress() converte o radiano para graus de 0 a 360.
+                    // e a função Math.abs() garante que meu valor sempre será positivo
+                    degrees = Math.abs(Math.toDegrees(Math.asin(sin))) + 90d;
                 }
             }
+            // se não, o cateto oposto é o deslocamento de X
+            else {
+                // dividindo o cateto oposto pela hipetenusa temos o Seno do Angulo que deve ser rotacionado.
+                double senAngulo = posTratorPrev.cartesian(0) / deslocamento;
+                Log.d(getClass().getSimpleName(), "senAngulo: " + senAngulo);
 
-            canvas.rotate((float) angulo);
+                // a função Math.asin() calcula o arco radiano para o Seno calculado anteriormente.
+                // e a função Math.toDegress() converte o radiano para graus de 0 a 360.
+                degrees = Math.toDegrees(Math.asin(senAngulo));
+            }
+
+            Log.d(getClass().getSimpleName(), "angulo: " + degrees);
+
+            canvas.rotate((float) degrees);
             canvas.drawPath(path, paint);
         }
-    }
-
-    private double radToDeg(double rad) {
-        return rad * (180d / Math.PI);
     }
 
     /**
